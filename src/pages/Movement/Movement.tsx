e81d77493c84144fb01eacd1fdfbf37a105a26b4
Yuri <sd.rolandi@gmail.com>
2021-01-15 22:08:56 -0300

    feat: melhorando visual

--- src/pages/Movement/Movement.tsx
+++ src/pages/Movement/Movement.tsx
@@ -10,16 +10,12 @@ function Movement(props: any) {
   const [tipo, setTipo] = useState<string>(" ");
   const [valor, setValor] = useState<string>("");
   useEffect(() => {
-    
     api
       .get(`movimentacoes/${props.match.params.data}.json`)
       .then((response) => {
         setMovement(response.data);
       });
   }, [props.match.params.data]);
-
-
-  
 
   function onChangeDescricao(evt: any): void {
     setDescricao(evt.target.value);
@@ -48,51 +44,57 @@ function Movement(props: any) {
       <SideBar />
       <div className="container">
         <h1>Movimentação</h1>
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Nome</th>
-              <th>Valor</th>
-              <th>Tipo</th>
-            </tr>
-          </thead>
+        <div className="box">
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Nome</th>
+                <th>Valor</th>
+                <th>Tipo</th>
+              </tr>
+            </thead>
 
-          <tbody>
-            {movement &&
-              Object.keys(movement).map((move) => {
-                return (
-                  <tr key={move}>
-                    <td>{movement[move].nome}</td>
-                    <td>{movement[move].valor}</td>
-                    <td>
-                      {movement[move].tipo}
-                      <button
-                        className="btn btn-danger mr-1 ml-2"
-                        onClick={() => remove(move)}
-                      >
-                        <FaTrash />
-                      </button>
-                    </td>
-                  </tr>
-                );
-              })}
+            <tbody>
+              {movement &&
+                Object.keys(movement).map((move) => {
+                  return (
+                    <tr key={move}>
+                      <td>{movement[move].nome}</td>
+                      <td>{movement[move].valor}</td>
+                      <td>
+                        {movement[move].tipo}
+                        <button
+                          className="btn btn-danger mr-1 ml-2"
+                          onClick={() => remove(move)}
+                        >
+                          <FaTrash />
+                        </button>
+                      </td>
+                    </tr>
+                  );
+                })}
 
-            <tr>
-              <td>
-                <input type="text" onChange={onChangeDescricao} value={nome} />
-              </td>
-              <td>
-                <input type="text" onChange={onChangeValor} value={valor} />
-              </td>
-              <td>
-                <input type="text" onChange={onChangeTipo} value={tipo} />
-                <button className="btn btn-success ml-2" onClick={save}>
-                  Salvar
-                </button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+              <tr>
+                <td>
+                  <input
+                    type="text"
+                    onChange={onChangeDescricao}
+                    value={nome}
+                  />
+                </td>
+                <td>
+                  <input type="text" onChange={onChangeValor} value={valor} />
+                </td>
+                <td>
+                  <input type="text" onChange={onChangeTipo} value={tipo} />
+                  <button className="btn btn-success ml-2" onClick={save}>
+                    Salvar
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
