674134745e5e3178198f90591cfa14ee29c66395
Rolandi <sd.rolandi@gmail.com>
2021-01-16 15:55:32 -0300

    feat: adicionando async em Get

--- src/hooks/useGet.tsx
+++ src/hooks/useGet.tsx
@@ -7,14 +7,19 @@ function useGet(url: string) {
   const [data, dispatch] = useReducer(reducer, {
     data: {},
   });
-  useEffect(() => {
+  const carregar = async () => {
     dispatch({
       type: "REQUEST",
     });
-    api.get(`${url}.json`).then((response) => {
-      dispatch({ type: "SUCCESS", data: response.data });
-    });
+    const response = await api.get(`${url}.json`);
+    dispatch({ type: "SUCCESS", data: response.data });
+  };
+  useEffect(() => {
+    carregar();
   }, [url]);
-  return data;
+  return {
+    ...data,
+    refetch: carregar
+  };
 }
 export default useGet;
