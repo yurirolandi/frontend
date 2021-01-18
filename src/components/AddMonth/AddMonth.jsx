ebf3d96f4d77a66b84a058c64e8cc3fe74ab443e
Rolandi <sd.rolandi@gmail.com>
2021-01-18 13:15:56 -0300

    feat: add dia

--- src/components/AddMonth/AddMonth.jsx
+++ src/components/AddMonth/AddMonth.jsx
@@ -4,7 +4,7 @@ import usePost from "../../hooks/usePost";
 
 export default function AddMonth() {
   const meses = [];
-  const [dia, setDia] = useState("");
+  const [dia, setDia] = useState("01");
   const [postData, salvar] = usePost(`meses/2021-${dia}`);
   const data = useGet(`meses`);
 
@@ -22,19 +22,29 @@ export default function AddMonth() {
   }
 
   return (
-    <>
+    <div className="box mb-3">
       <h2>Adicionar Mês</h2>
-      <select>
-        <option value="2021">2021</option>
-      </select>
-      <select value={dia} onChange={(e) => setDia(e.target.value)}>
-        {meses.map((mes) => (
-          <option key={mes} value={mes}>
-            {mes}
-          </option>
-        ))}
-      </select>
-      <button onClick={addMes}>Adicionar Mês</button>
-    </>
+
+      <div className="container d-flex justify-content-center align-items-center">
+        <select className="form-select m-2 p-2">
+          <option value="2021">2021</option>
+        </select>
+
+        <select
+          className="form-select m-2 p-2"
+          value={dia}
+          onChange={(e) => setDia(e.target.value)}
+        >
+          {meses.map((mes) => (
+            <option key={mes} value={mes}>
+              {mes}
+            </option>
+          ))}
+        </select>
+        <button className="btn btn-success m-2" onClick={addMes}>
+          Adicionar Mês
+        </button>
+      </div>
+    </div>
   );
 }
