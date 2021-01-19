070e67748fa9ff5caf973024b6e45d85dbeaf8d8
Rolandi <sd.rolandi@gmail.com>
2021-01-19 18:20:13 -0300

    fix: removendo rota do dashboard

--- src/routes.js
+++ src/routes.js
@@ -2,15 +2,13 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import Landing from "./pages/Landing/Landing";
-import Dashboard from './pages/Dashboard/Dashboard';
-import Movement from './pages/Movement/Movement';
+import Movement from "./pages/Movement/Movement";
 
 function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Landing} />
-        <Route path="/dashboard" component={Dashboard} />
         <Route path="/movement/:data" component={Movement} />
       </Switch>
     </BrowserRouter>
