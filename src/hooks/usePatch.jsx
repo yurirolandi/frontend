13046ce65785615b3bd7e4b2a9939101ba95850d
Rolandi <sd.rolandi@gmail.com>
2021-01-19 08:08:32 -0300

    feat: user path

---
+++ src/hooks/usePatch.jsx
@@ -0,0 +1,21 @@
+
+import React, { useReducer } from "react";
+import reducer from "./rest";
+import api from "../services/api";
+
+function usePatch() {
+  const [data, dispatch] = useReducer(reducer, {
+    data: {},
+  });
+  const patch = async (url, data) => {
+    dispatch({
+      type: "REQUEST",
+    });
+    await api.patch(`${url}.json`, data);
+    dispatch({
+      type: "SUCCESS",
+    });
+  };
+  return [data, patch];
+}
+export default usePatch;
