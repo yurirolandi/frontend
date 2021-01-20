13496b7354c966419056057b97972f7434c04012
Rolandi <sd.rolandi@gmail.com>
2021-01-20 14:27:54 -0300

    feat: table improve

--- src/components/AddMonth/AddMonth.jsx
+++ src/components/AddMonth/AddMonth.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import OptionsValues from "../Options/Options";
 import useGet from "../../hooks/useGet";
 import usePost from "../../hooks/usePost";
+import Table from "../Table/Table";
 
 export default function AddMonth() {
   const [dia, setDia] = useState("01");
@@ -10,7 +11,8 @@ export default function AddMonth() {
   const [show, setShow] = useState(false);
 
   async function addMes() {
-    if (Object.keys(data.data)[0] === `2021-${dia}`) return setShow(true);
+    if (data.data && Object.keys(data.data)[0] === `2021-${dia}`)
+      return setShow(true);
     await salvar({
       saida: 0,
     });
@@ -18,41 +20,44 @@ export default function AddMonth() {
   }
 
   return (
-    <div className="box mb-3 mt-5">
-      <h2 className="mb-4 text-center">Adicionar Mês</h2>
+    <>
+      <div className="box mb-3 mt-5">
+        <h2 className="mb-4 text-center">Adicionar Mês</h2>
 
-      <div className="container d-flex justify-content-center align-items-center">
-        <select className="form-select m-2 p-2">
-          <option value="2021">2021</option>
-        </select>
+        <div className="container d-flex justify-content-center align-items-center">
+          <select className="form-select m-2 p-2">
+            <option value="2021">2021</option>
+          </select>
 
-        <select
-          className="form-select m-2 p-2"
-          value={dia}
-          onChange={(e) => setDia(e.target.value)}
-        >
-          <OptionsValues />
-        </select>
-        <button className="btn btn-success m-2" onClick={addMes}>
-          Adicionar Mês
-        </button>
-      </div>
-
-      {show && (
-        <div className="warning">
-          <h5>
-            Mês já adicionado!{" "}
-            <button className="close" onClick={() => setShow(false)}>
-              x
-            </button>
-          </h5>
-
-          <p>
-            Me parece que esse mês já foi adicionado na sua lista, tente inserir
-            outro mês...
-          </p>
+          <select
+            className="form-select m-2 p-2"
+            value={dia}
+            onChange={(e) => setDia(e.target.value)}
+          >
+            <OptionsValues />
+          </select>
+          <button className="btn btn-success m-2" onClick={addMes}>
+            Adicionar Mês
+          </button>
         </div>
-      )}
-    </div>
+
+        {show && (
+          <div className="warning">
+            <h5>
+              Mês já adicionado!{" "}
+              <button className="close" onClick={() => setShow(false)}>
+                x
+              </button>
+            </h5>
+
+            <p>
+              Me parece que esse mês já foi adicionado na sua lista, tente
+              inserir outro mês...
+            </p>
+          </div>
+        )}
+      </div>
+      <Table props={data} />
+    </>
   );
 }
