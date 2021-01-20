1cf574547628127a9d7bbbf6822cd3debb7d8160
Rolandi <sd.rolandi@gmail.com>
2021-01-20 13:56:03 -0300

    feat: add patch, responsive table and plus the values output

--- src/pages/Movement/Movement.jsx
+++ src/pages/Movement/Movement.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import SideBar from "../../components/SideBar/SideBar";
 import OptionsValues from "../../components/Options/Options";
 import useGet from "../../hooks/useGet";
 import usePost from "../../hooks/usePost";
 import useDelete from "../../hooks/useDelete";
+import usePatch from "../../hooks/usePatch";
 import { FaTrash } from "react-icons/fa";
 
 function Movement(props) {
@@ -13,10 +14,15 @@ function Movement(props) {
   const [postData, salvar] = usePost(
     `movimentacoes/${props.match.params.data}`
   );
+
+  const [dataPatch, patch] = usePatch();
   const [removeData, remover] = useDelete();
   const [nome, setDescricao] = useState("");
   const [valor, setValor] = useState("");
   const [show, setShow] = useState(false);
+  const [total, setTotal] = useState("");
+
+  const reducer = (accumulator, currentValue) => accumulator + currentValue;
 
   async function save() {
     if (nome !== "" && valor !== "") {
@@ -25,16 +31,32 @@ function Movement(props) {
         valor,
         parcelas,
       });
+      data.refetch();
+      dataMeses.refetch();
       setDescricao("");
       setValor("");
-      data.refetch();
-      setTimeout(() => {
-        dataMeses.refetch();
-      }, 10000);
     } else {
       return setShow(true);
     }
   }
+
+  async function valorTotal() {
+    let value = [];
+    if (data.data) {
+      Object.keys(data.data).forEach((valor) => {
+        value.push(parseInt(data.data[valor].valor));
+      });
+    }
+    if (value.length !== 0) {
+      let total = value.reduce(reducer); 
+      setTotal(total);
+    }
+    return;
+  }
+
+  useEffect(() => {
+    valorTotal();
+  }, [data.refetch]);
 
   async function remove(id) {
     await remover(`movimentacoes/${props.match.params.data}/${id}`);
@@ -44,76 +66,86 @@ function Movement(props) {
     }, 10000);
   }
 
+  function saveTotal() {
+    let id = Object.keys(dataMeses.data)[0]
+    patch(`meses/${props.match.params.data}/${id}`, {
+      saida: total
+    });
+  }
+
   return (
     <div className="movement">
       <SideBar />
       <div className="container mt-5">
         <div className="box">
-          <h1 className="mb-4 text-center">Movimentação</h1>
-          <table className="table">
-            <thead>
-              <tr>
-                <th>Nome</th>
-                <th>Valor</th>
-                <th>Parcelas</th>
-                <th> </th>
-              </tr>
-            </thead>
+          <h1 className="mb-4 text-center">Movimenta&shy;ção</h1>
+          <div className="table-responsive">
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>Nome</th>
+                  <th>Valor</th>
+                  <th>Parcelas</th>
+                  <th> </th>
+                </tr>
+              </thead>
 
-            <tbody>
-              {data.data &&
-                Object.keys(data.data).map((move) => {
-                  return (
-                    <tr key={move}>
-                      <td>{data.data[move].nome}</td>
-                      <td>{data.data[move].valor}</td>
-                      <td>{data.data[move].parcelas}</td>
-                      <td>
-                        <button
-                          className="btn btn-danger ml-2"
-                          onClick={() => remove(move)}
-                        >
-                          <FaTrash />
-                        </button>
-                      </td>
-                    </tr>
-                  );
-                })}
+              <tbody>
+                {data.data &&
+                  Object.keys(data.data).map((move) => {
+                    return (
+                      <tr key={move}>
+                        <td>{data.data[move].nome}</td>
+                        <td>{data.data[move].valor}</td>
+                        <td>{data.data[move].parcelas}</td>
+                        <td>
+                          <button
+                            className="btn btn-danger ml-2"
+                            onClick={() => remove(move)}
+                          >
+                            <FaTrash />
+                          </button>
+                        </td>
+                      </tr>
+                    );
+                  })}
 
-              <tr>
-                <td>
-                  <input
-                    type="text"
-                    onChange={(evt) => setDescricao(evt.target.value)}
-                    value={nome}
-                    size={8}
-                  />
-                </td>
-                <td>
-                  <input
-                    type="text"
-                    onChange={(evt) => setValor(evt.target.value)}
-                    value={valor}
-                    size={8}
-                  />
-                </td>
-                <td>
-                  <select
-                    className="form-select p-1"
-                    onChange={(evt) => setParcelas(evt.target.value)}
-                    value={parcelas}
-                  >
-                    <OptionsValues />
-                  </select>
-                </td>
-                <td>
-                  <button className="btn btn-success" onClick={save}>
-                    Salvar
-                  </button>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+                <tr>
+                  <td>
+                    <input
+                      type="text"
+                      className="form-movement"
+                      onChange={(evt) => setDescricao(evt.target.value)}
+                      value={nome}
+                    />
+                  </td>
+                  <td>
+                    <input
+                      type="number"
+                      className="form-movement"
+                      onChange={(evt) => setValor(evt.target.value)}
+                      value={valor}
+                    />
+                  </td>
+                  <td>
+                    <select
+                      className="form-select p-1"
+                      onChange={(evt) => setParcelas(evt.target.value)}
+                      value={parcelas}
+                    >
+                      <OptionsValues />
+                    </select>
+                  </td>
+                  <td>
+                    <button className="btn btn-success" onClick={save}>
+                      Salvar
+                    </button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            <button className="btn btn-primary" onClick={saveTotal}>Finalizar </button>
+          </div>
           {show && (
             <div className="warning">
               <h5>
@@ -123,9 +155,7 @@ function Movement(props) {
                 </button>
               </h5>
 
-              <p>
-                Inclua nome e valor!!!
-              </p>
+              <p>Inclua nome e valor!!!</p>
             </div>
           )}
         </div>
