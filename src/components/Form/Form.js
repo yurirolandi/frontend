f1da839c30c0787d107410ce72ced033f6b97286
Rolandi <sd.rolandi@gmail.com>
2021-01-23 08:47:34 -0300

    feat: form login

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -3,81 +3,56 @@ import "./form.css";
 import api from "../../services/api";
 
 export default function Form() {
-
-  const [name, setName] = useState("");
-  const [price, setPrice] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [type, setType] = useState("");
-  const [descriptions, setDescripitions] = useState("");
 
-  async function handlerSubmit(event: FormEvent) {
+  async function handlerSubmit(event) {
     event.preventDefault();
 
-    const data = {
-      name: name,
-      price: price,
-      type: type,
-      descriptions: descriptions,
-    };
+    // const data = {
+    //   name: name,
+    //   price: price,
+    //   type: type,
+    //   descriptions: descriptions,
+    // };
 
-    console.log("descriptions", data);
-    await api.post("/product", data);
+    // console.log("descriptions", data);
+    // await api.post("/product", data);
 
     //history.push("/product");
   }
 
   return (
     <div className="box" onSubmit={handlerSubmit}>
-      <form className="create-form">
-        <fieldset>
-          <legend>Dados</legend>
+      <div className="box-container">
+        <form className="create-form">
+          <fieldset>
+            <legend className="text-center">Login</legend>
 
-          <div className="input-block">
-            <label htmlFor="name">Nome da Compra</label>
-            <input
-              className="name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
+            <div className="input-block">
+              <label htmlFor="email">Email:</label>
+              <input
+                className="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
 
-            <label htmlFor="valor">Valor da Compra</label>
-            <input
-              type="number"
-              className="valor"
-              value={price}
-              onChange={(e) => setPrice(e.target.value)}
-            />
+              <label htmlFor="password">Senha:</label>
+              <input
+                type="password"
+                className="valor"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+          </fieldset>
 
-            <label htmlFor="tipo">Tipo de comercio</label>
-            <select
-              className="tipo"
-              value={type}
-              onChange={(e) => setType(e.target.value)}
-            >
-              <option value="outros">Outros</option>
-              <option value="cartao">Cartão</option>
-              <option value="mercado">Compras de mercado</option>
-              <option value="roupas">Roupas</option>
-              <option value="aluguel">Aluguel</option>
-            </select>
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="descricao">
-              Descrição <span>Máximo de 300 caracteres</span>
-            </label>
-            <textarea
-              className="name"
-              maxLength={300}
-              value={descriptions}
-              onChange={(e) => setDescripitions(e.target.value)}
-            />
-          </div>
-        </fieldset>
-
-        <button className="confirm-button" type="submit">
-          Confirmar
-        </button>
-      </form>
+          <button className="confirm-button" type="submit">
+            Sign in
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
