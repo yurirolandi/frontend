629c6323f86022f0da4478cf31d60e30a68dd445
Rolandi <sd.rolandi@gmail.com>
2021-01-23 08:47:23 -0300

    feat: router login

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,13 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import Landing from "./pages/Landing/Landing";
 import Movement from "./pages/Movement/Movement";
+import Login from './pages/Login/Login';
 
 function Routes() {
   return (
     <BrowserRouter>
       <Switch>
+        <Route path="/login" exact component={Login} />
         <Route path="/" exact component={Landing} />
         <Route path="/movement/:data" component={Movement} />
       </Switch>
