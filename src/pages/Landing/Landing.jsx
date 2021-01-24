ca0195d7a59d08d936c504fe84cc801f67ebf5a9
Rolandi <sd.rolandi@gmail.com>
2021-01-24 15:55:57 -0300

    fix; comments cods

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useHistory } from "react-router-dom";
 import SideBar from "../../components/SideBar/SideBar";
 import AddMonth from "../../components/AddMonth/AddMonth";
 
 function Landing() {
-  
+  // const history = useHistory();
+  // const logado = () => {
+  //   const token = localStorage.getItem("token");
+  //   if (!token) history.push("/login");
+  // };
+
+  // useEffect(() => {
+  //   logado();
+  // });
+
   return (
     <div className="page-landing">
       <SideBar />
