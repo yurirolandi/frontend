f4686f4194735f1f8b066eb56a28871386d77f08
Rolandi <sd.rolandi@gmail.com>
2021-01-24 15:53:43 -0300

    feat: add router login and register

--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,13 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Landing from "./pages/Landing/Landing";
 import Movement from "./pages/Movement/Movement";
 import Login from './pages/Login/Login';
+import createLogin from './pages/Login/CreateLogin';
 
 function Routes() {
   return (
     <BrowserRouter>
       <Switch>
+        <Route path="/login/create" exact component={createLogin} />
         <Route path="/login" exact component={Login} />
         <Route path="/" exact component={Landing} />
         <Route path="/movement/:data" component={Movement} />
