7e4552313cdff3a9aceb53bdf36fdc4a1ac78c8a
Rolandi <sd.rolandi@gmail.com>
2021-01-24 11:46:07 -0300

    feat: login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,11 +1,8 @@
 import React from "react";
-import usePostToken from "../../hooks/usePostToken";
 import Form from "../../components/Form/Form";
-import './login.css'
+import "./login.css";
 
 export default function Login() {
-  const [postData, signin] = usePostToken(`movimentacoes/`);
-
   return (
     <div className="login">
       <div className="container">
